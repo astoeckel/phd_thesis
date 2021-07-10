9c9679a18d2c148084297bc5aee2ee8f8bf57aab
Andreas Stöckel <astoecke@uwaterloo.ca>
2021-07-10 16:52:04 -0400

    Once again correct the main/child process detection code

--- code/lib/two_comp_parameters.py
+++ code/lib/two_comp_parameters.py
@@ -401,7 +401,7 @@ def in_notebook():
 if in_notebook():
     N_SOLVER_THREADS = multiprocessing.cpu_count() // 2
 else:
-    if hasattr(multiprocessing, "parent_process") and (multiprocessing.parent_process() is None):
+    if hasattr(multiprocessing, "current_process") and (multiprocessing.current_process().name == "MainProcess"):
         N_SOLVER_THREADS = multiprocessing.cpu_count() // 2
     else:
         N_SOLVER_THREADS = 1 # We're already parallelising over experiments
